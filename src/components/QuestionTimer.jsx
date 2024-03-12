dcefc9846d8688d6e6784aeb8520c315e0178faa
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-03-12 14:01:39 +0200

    last-commit

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,23 +1,34 @@
 import React, {useEffect, useState} from 'react';
 
-const QuestionTimer = ({timeout,onTimeout}) => {
+const QuestionTimer = ({timeout,onTimeout,mode}) => {
     const [remainingTime, setRemainingTime] = useState(timeout)
 
     useEffect(() => {
-        setTimeout(onTimeout,timeout)
+        console.log('SETTING TIMEOUT')
+        const timer = setTimeout(onTimeout,timeout)
+
+        return () => {
+            clearTimeout(timer)
+        }
     },[timeout,onTimeout ])
 
 
     useEffect(() => {
-        setInterval(() => {
+        console.log('SETTING TIMEOUT')
+       const interval = setInterval(() => {
             setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
         },100)
+
+        return () => {
+            clearInterval(interval)
+        }
+
     },[])
 
 
 
     return (
-        <progress id="question-time" max={timeout} value={remainingTime }/>
+        <progress id="question-time" max={timeout} value={remainingTime} className={mode}/>
     );
 };
 
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,48 +1,43 @@
-import React, {useState} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 
 
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png'
 import QuestionTimer from "./QuestionTimer.jsx";
+import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 const Quiz = () => {
     const [userAnswers, setUserAnswer] = useState([])
 
-    const activeQuestioIndex = userAnswers.length
-
+    const activeQuestioIndex =  userAnswers.length
     const quizIsComplete = activeQuestioIndex === QUESTIONS.length
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswer((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer]
         })
-    }
+
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [])
 
     if (quizIsComplete) {
         return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy icon"/>
-                <h2>Quiz Completed!</h2>
-            </div>
+           <Summary userAnswers={userAnswers}/>
         )
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestioIndex].answers]
-    shuffledAnswers.sort(() => Math.random() - 0.5)
 
     return (
         <div id="quiz">
-            <div id="question">
-                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
-                <h2>{QUESTIONS[activeQuestioIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map((answer) =>
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    )}
-                </ul>
-            </div>
+            <Question
+                key={activeQuestioIndex}
+                index={activeQuestioIndex}
+                onSelectAnswer={handleSelectAnswer}
+                onSkipAnswer={handleSkipAnswer }
+             />
         </div>
     );
 };
